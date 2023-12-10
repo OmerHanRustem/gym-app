f68784d47ce05eda1d3c955c88ee061e389507cd
OmerHanRustem <omer.han.1431@gmail.com>
2023-12-10 17:06:49 +0200

    1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
